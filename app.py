6015e353aca34a577552d48dcf880a931d54e001
suhas-naik <147157708+suhas-naik@users.noreply.github.com>
2024-07-08 21:29:44 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
-from htmlTemplates import css, bot_template, user_template
+from htmlTemplates.py import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
 
 def get_pdf_text(pdf_docs):
